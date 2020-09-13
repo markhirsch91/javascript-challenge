671febccb35e676620c5c40edf0ba695240e7abe
Mark Hirschenberger <markhirsch91@gmail.com>
2020-09-13 18:50:03 -0400

    First function is complete

--- JavaScript and DOM Manipulation/static/js/app.js
+++ JavaScript and DOM Manipulation/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+
+// This function will append the table with the UFO Sightings from "data.js"
+function ufoTable(data) {
+    // Ensure that the table is cleared out before we begin the loop
+    tbody.html("");
+    // For every report of a UFO sighting in data.js, add it to a new row in the "tbody" table.
+    data.forEach(ufoReport => {
+       tableRow = tbody.append("tr");
+       // This is where we will iterate through each key/value pair
+        Object.entries(ufoReport).forEach(function([key, value]) {
+            tableDataLine = tableRow.append("td").text(value);
+        })
+    })
+}
+// Apply the function above with the tableData variable.
+ufoTable(tableData);
+
